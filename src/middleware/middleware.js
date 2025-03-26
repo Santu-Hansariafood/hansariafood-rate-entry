93d53ba08d0b2a87abdfd106e32005de80fd2acb
Santu-Hansariafood <Santu@hansariafood.com>
2025-03-26 11:10:49 +0530

    added code with seo

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -7,5 +7,5 @@ export default withAuth({
 });
 
 export const config = {
-  matcher: ["/", "/company", "/rate", "/manage", "/location", "/category"],
+  matcher: ["/", "/company", "/rate", "/manage", "/location", "/category", "/register"],
 };
