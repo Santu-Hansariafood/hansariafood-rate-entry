7e67366b754f810c58c542faa3ec110ffab20a33
Santu-Hansariafood <Santu@hansariafood.com>
2025-08-29 16:57:55 +0530

    loading is missing so update accordingly

--- src/components/ui/TopSaudaList/ReportGraph/BarChartComponent.jsx
+++ src/components/ui/TopSaudaList/ReportGraph/BarChartComponent.jsx
@@ -11,7 +11,7 @@ import {
   Legend,
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
-
+import Loading from "@/components/common/Loading/Loading";
 Chart.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const BarChartComponent = ({ data }) => {
