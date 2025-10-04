b91b1cce6a2dbe96026d149cdfc9bc0d54db8819
Santu-Hansariafood <Santu@hansariafood.com>
2025-10-04 11:40:24 +0530

    added here auth wrapper in the set

--- src/app/selfcompany/page.jsx
+++ src/app/selfcompany/page.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import SelfCompany from '@/components/ui/SelfCompany/SelfCompany'
+import dynamic from "next/dynamic";
+import React from "react";
+const SelfCompany = dynamic(() =>
+  import("@/components/ui/SelfCompany/SelfCompany")
+);
+const AuthWrapper = dynamic(() =>
+  import("@/components/AuthWrapper/AuthWrapper")
+);
 
 const page = () => {
   return (
-    <div>
-      <SelfCompany />
-    </div>
-  )
-}
+    <AuthWrapper>
+      <section role="region" aria-label="Self Company Management Section">
+        <SelfCompany />
+      </section>
+    </AuthWrapper>
+  );
+};
 
-export default page
+export default page;
