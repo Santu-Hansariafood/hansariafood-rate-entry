0acf16a88e2689dea8e8742976f28c2180c3ef8d
Santu-Hansariafood <Santu@hansariafood.com>
2026-02-07 14:48:23 +0530

    update google add manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
@@ -130,6 +131,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5VPLHWSD');`}
+        </Script>
         <meta name="geo.region" content="IN-WB" />
         <meta name="geo.placename" content="Kolkata" />
         <meta name="distribution" content="global" />
@@ -185,6 +193,14 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased flex flex-col min-h-screen`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5VPLHWSD"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <AuthProvider>
           <AuthWrapper>
             <UserProvider>
