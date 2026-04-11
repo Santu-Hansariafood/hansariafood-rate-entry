4fd66f5d27af6ed010bc14e6745ca417814c0a07
Santu-Hansariafood <Santu@hansariafood.com>
2026-04-11 16:47:32 +0530

    update the server with port no

--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import { Server } from "socket.io";
 import { setIO } from "./src/lib/socket.js";
 
 const dev = process.env.NODE_ENV !== "production";
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 16000;
 
 const app = next({ dev, port });
 const handle = app.getRequestHandler();
